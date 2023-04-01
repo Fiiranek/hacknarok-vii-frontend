d0a77be8524a512e0033f99d0b7cbdaa0a53491e
Wenszel <wiktor.smaga13@gmail.com>
2023-04-01 13:29:52 +0200

    added login screen

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Start from './src/screens/start/Start';
-const Tab = createBottomTabNavigator();
+import { useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Provider as PaperProvider } from 'react-native-paper'
+import Start from './src/screens/start/Start'
+import Login from './src/screens/Login/Login'
+const Tab = createBottomTabNavigator()
 export default function App() {
-  return (
-
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Start} />
-
+  const Stack = createStackNavigator()
+  function HomeTabs() {
+    return (
+      <Tab.Navigator options={{ headerShown: false }}>
+        <Tab.Screen
+          name="Home"
+          component={Start}
+          options={{ headerShown: false }}
+        />
       </Tab.Navigator>
-    </NavigationContainer>
-
-
-  );
+    )
+  }
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            options={{ headerShown: false }}
+            component={Login}></Stack.Screen>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Home"
+            component={HomeTabs}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -25,4 +46,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
