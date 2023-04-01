30d58921a626b0ac90ff83a4d8069819723be88d
Franciszek Job <jellybean@onet.eu>
2023-04-01 11:06:17 +0200

    added global styles

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Start from './src/screens/start/Start';
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Start} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+
+
   );
 }
 
---
+++ globalStyle.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+module.exports = StyleSheet.create({
+    colors: {
+        primaryBlack: "#1F2421",
+        primaryAqua: "#216869",
+        primaryGreen: "#49A078",
+        primaryLightGreen: "#9CC5A1",
+        primaryPlatinium: "DCE1DE",
+    },
+});
