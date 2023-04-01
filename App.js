aba512de82c107e5731bf3bfeb60a6946cd0bcd2
Wenszel <wiktor.smaga13@gmail.com>
2023-04-01 14:05:50 +0200

    added map screen

--- App.js
+++ App.js
@@ -1,74 +1,67 @@
-import { useState } from 'react';
-import Home from './src/screens/home/Home';
-import { StyleSheet, Text, View } from 'react-native';
-import { BottomNavigation, } from 'react-native-paper';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { Provider as PaperProvider, MD3LightTheme as DefaultTheme, } from 'react-native-paper';
-import Profile from './src/screens/profile/Profile';
+import { useState } from "react";
+import Home from "./src/screens/home/Home";
+import { StyleSheet, Text, View } from "react-native";
+import { BottomNavigation } from "react-native-paper";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { Provider as PaperProvider, MD3LightTheme as DefaultTheme } from "react-native-paper";
+import Profile from "./src/screens/profile/Profile";
+//import Map from "./src/screens/map/Map";
 
 const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primaryBlack: "#1F2421",
-    primaryAqua: "#216869",
-    primaryGreen: "#49A078",
-    primaryLightGreen: "#9CC5A1",
-    primaryPlatinium: "DCE1DE",
-  },
-  text: {
-
-    h1: {
-      ...DefaultTheme.text,
-      fontSize: 20
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        primaryBlack: "#1F2421",
+        primaryAqua: "#216869",
+        primaryGreen: "#49A078",
+        primaryLightGreen: "#9CC5A1",
+        primaryPlatinium: "DCE1DE",
     },
-    h2: {
-      ...DefaultTheme.text,
-      fontSize: 16
-    }
-  }
+    text: {
+        h1: {
+            ...DefaultTheme.text,
+            fontSize: 20,
+        },
+        h2: {
+            ...DefaultTheme.text,
+            fontSize: 16,
+        },
+    },
 };
 
-
 export default function App() {
-  const [index, setIndex] = useState(0)
-  const [routes] = useState([
-    { key: 'home', title: 'Favorites', focusedIcon: 'home', unfocusedIcon: 'home-outline' },
-    { key: 'maps', title: 'Maps', focusedIcon: 'map', unfocusedIcon: 'map-outline' },
-    { key: 'scan', title: 'Scan', focusedIcon: 'qr-code', unfocusedIcon: 'qr-code-outline' },
+    const [index, setIndex] = useState(0);
+    const [routes] = useState([
+        { key: "home", title: "Favorites", focusedIcon: "home", unfocusedIcon: "home-outline" },
+        { key: "maps", title: "Maps", focusedIcon: "map", unfocusedIcon: "map-outline" },
+        { key: "scan", title: "Scan", focusedIcon: "qr-code", unfocusedIcon: "qr-code-outline" },
 
-    { key: 'ranks', title: 'Ranks', focusedIcon: 'trophy', unfocusedIcon: 'trophy-outline' },
-    { key: 'profile', title: 'Profile', focusedIcon: 'person', unfocusedIcon: 'person-outline' },
-  ]);
+        { key: "ranks", title: "Ranks", focusedIcon: "trophy", unfocusedIcon: "trophy-outline" },
+        { key: "profile", title: "Profile", focusedIcon: "person", unfocusedIcon: "person-outline" },
+    ]);
 
-  const renderScene = BottomNavigation.SceneMap({
-    home: Home,
-    profile: Profile
+    const renderScene = BottomNavigation.SceneMap({
+        home: Home,
+        profile: Profile,
+    });
 
-  });
-
-  return (
-    <PaperProvider theme={theme}
-      settings={{
-        icon: props => <Ionicons {...props} />,
-      }}>
-      <BottomNavigation
-
-
-        navigationState={{ index, routes }}
-        onIndexChange={setIndex}
-        renderScene={renderScene}
-      />
-    </PaperProvider>
-
-  );
+    return (
+        <PaperProvider
+            theme={theme}
+            settings={{
+                icon: props => <Ionicons {...props} />,
+            }}
+        >
+            <BottomNavigation navigationState={{ index, routes }} onIndexChange={setIndex} renderScene={renderScene} />
+        </PaperProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
