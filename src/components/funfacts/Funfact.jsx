4199eee2827471df34223160aae37e2db8782270
Wenszel <wiktor.smaga13@gmail.com>
2023-04-02 06:51:46 +0200

    merged

---
+++ src/components/funfacts/Funfact.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import { Dimensions, View, Text, Image } from 'react-native';
+import { useTheme, List } from 'react-native-paper';
+import lightbulb from '../../../assets/lightbulb.png';
+import facts from './facts';
+export default () => {
+    const theme = useTheme();
+    const getRandomInt = max => {
+        return Math.floor(Math.random() * max);
+    };
+    const drawFunfact = () => {
+        let random = getRandomInt(facts.length);
+        return facts[random].text;
+    };
+    const [caption, setCaption] = React.useState(drawFunfact);
+    return (
+        <View
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: 20,
+
+                margin: 5,
+                backgroundColor: 'white',
+                height: Dimensions.get('window').height * 0.15,
+                width: Dimensions.get('window').width * 0.95,
+                padding: 10,
+            }}
+        >
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                <Image style={{ width: 20, height: 20 }} source={lightbulb}></Image>
+                <Text style={{ ...theme.text.h1, alignSelf: 'left', marginLeft: 20 }}>Did you know?</Text>
+            </View>
+
+            <Text style={theme.text.h2}>{caption}</Text>
+        </View>
+    );
+};
---
+++ src/components/funfacts/facts.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        text: '100 Billion Items of Clothing Are Produced Each Year',
+    },
+    {
+        text: 'The Average Person Only Wears 20% of Their Clothes 80% of the Time',
+    },
+    {
+        text: 'The Target Audience for Fast Fashion Retailers Is Largely Consumers Aged 18 to 24 ',
+    },
+    {
+        text: 'Fast Fashion Companies Generate More Pollution Than International Aviation and Shipping Combined ',
+    },
+    {
+        text: '60% of Clothes Are Made With Plastic-based Materials',
+    },
+    {
+        text: 'The Fashion Industry Consumes Around 93 Billion Cubic Metres of Water Each Year',
+    },
+    {
+        text: 'We Discard 92 Million Tonnes of Textile Waste That the Industry Generates Annually',
+    },
+    {
+        text: 'More than USD$500 Billion Are Lost from Lack of Recycling and Clothing Underutilisation ',
+    },
+    {
+        text: '80% of Apparel is Made by Young Women Between the Ages of 18 and 24',
+    },
+    {
+        text: '59% of All Sustainability Claims by European Fashion Brands Are Inaccurate and Misleading',
+    },
+    {
+        text: 'The European Union is Moving to Tackle Fast Fashion Industries',
+    },
+];
